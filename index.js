12ebb68f5c2c658d7024adc942845c4c00785ef6
abajoseph <abajoseph940@gmail.com>
2026-08-28 13:37:52 +0800

    feat: express server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-{
-" Aba, Joseph Jr. A."
-"Section: C"
-}
+const express = require('express');
+const server = express();
+const port = 3000;
+
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
